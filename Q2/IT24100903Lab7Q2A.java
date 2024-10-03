ecbd3453f7031248a09b46b3fc76b4814b08a2f0
IT24100903 <it24100903@my.sliit.lk>
2024-10-03 23:37:07 +0530

    Added Q2A and Q2B

---
+++ Q2/IT24100903Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24100903Lab7Q2A
+ {
+    public static void main(String[] args) 
+	{
+		int count=0;
+		while(count<4)
+		{
+			System.out.println("$ $ $ $ $");
+			count++;
+		}
+	}
+ }
